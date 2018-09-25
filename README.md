0032737584a758ec6e5990c311f3ce7219416cc3
Anplus <an@tagsys.org>
2018-09-25 20:37:06 +0800

    signal

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RFID-PHY-Signal-Processing
+
+RFID Physical Layer Protocol
--- signal/gen_baseband_impinj_m2.m
+++ signal/gen_baseband_impinj_m2.m
@@ -1,24 +1,28 @@
-function output = gen_baseband(input, preamble)
+function output = gen_baseband_impinj_m2(input, preamble)
 %% GEN - 2 PHY
 %% PHY in us
 sample_rate = 2e6;
 samples_per_us = sample_rate/1e6;
+
 delim_len = 12*samples_per_us;
-TRcal_len = 200*samples_per_us;
-RTcal_len =72*samples_per_us;
-Tari_len = 24*samples_per_us;
-PW_len = 12*samples_per_us;
+% BLF = DR/TRcal = 64/3/(378/6*1e-6);
+RTcal_len = 378/6*samples_per_us;
+TRcal_len = 510/6*samples_per_us;
+
+Tari_len = 108/6*samples_per_us;
+PW_len = 54/6*samples_per_us;
+Data1_len = 174/6*samples_per_us;
 
 %% Wait time
 % T1 = max(10*Tpri,RTcal)=250us
 % T2 = [3Tpri,20Tpri] = 500us
 % T4 = 2*RTcal , 300 us
 % Tpri = 1/BLF = TRcal/DR = 25us
-cw_waitRN16_send = ones(1,(250+500+18*25)*samples_per_us);
+cw_waitRN16_send = ones(1,(700)*samples_per_us);
 cw_wait_send = ones(1,300*samples_per_us);
 
 %% basic time structure
-data_1_send = [ones(1,Tari_len*1.5),zeros(1,PW_len)];
+data_1_send = [ones(1,Data1_len-PW_len),zeros(1,PW_len)];
 data_0_send = [ones(1,Tari_len-PW_len),zeros(1,PW_len)];
 RTcal_send = [ones(1,RTcal_len-PW_len),zeros(1,PW_len)];
 TRcal_send = [ones(1,TRcal_len-PW_len),zeros(1,PW_len)];
@@ -47,10 +51,10 @@ end
 %% preamble
 if preamble == 1
 % need preamble, QUERY
-    command_send = [preamble_send,command_send,cw_waitRN16_send];
+    command_send = [preamble_send,command_send];
 else
 % need fram-sync, other command
-    command_send = [fram_send,command_send,cw_wait_send];
+    command_send = [fram_send,command_send];
 end
 %% wait Time
 output = command_send;
